aa002f197a779596a041d52ff5591038c19b02bc
IBArbitrary <k.rajeshkumar.1411@gmail.com>
2021-06-05 14:46:29 +0530

    addedall

---
+++ dmenu/config.h
@@ -0,0 +1,23 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"monospace:size=9"
+};
+static const char *prompt      = ">";      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#a89984", "#3c3836" },
+	[SchemeSel] = { "#282828", "#928374" },
+	[SchemeOut] = { "#282828", "#ebdbb2" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";

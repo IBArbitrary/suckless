2c7a59207b35e1058e1e101dcf30a85e868aef07
IBArbitrary <k.rajeshkumar.1411@gmail.com>
2021-06-17 11:08:22 +0530

    added fuzzymatch in dmenu

--- dmenu/config.h
+++ dmenu/config.h
@@ -1,22 +1,22 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 0; /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-	"monospace:style=Medium:size=9"
-};
-static const char *prompt      = ">";      /* -p  option; prompt to the left of input field */
+static const char *fonts[] = {"monospace:style=Medium:size=9"};
+static const char *prompt =
+    ">"; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#a89984", "#3c3836" },
-	[SchemeSel] = { "#282828", "#928374" },
-	[SchemeSelHighlight] = { "#fbf1c7", "#928374" },
-	[SchemeNormHighlight] = { "#fabd2f", "#3c3836" },
-	[SchemeOut] = { "#282828", "#ebdbb2" },
+    /*     fg         bg       */
+    [SchemeNorm] = {"#a89984", "#3c3836"},
+    [SchemeSel] = {"#282828", "#928374"},
+    [SchemeSelHighlight] = {"#fbf1c7", "#928374"},
+    [SchemeNormHighlight] = {"#fabd2f", "#3c3836"},
+    [SchemeOut] = {"#282828", "#ebdbb2"},
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines = 0;
 
 /*
  * Characters not considered part of a word while deleting words
